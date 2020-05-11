6b8c5b54109b90fb13667deeeb35b416e0cfe9cf
DarthFoley <59142754+QuiGonJames@users.noreply.github.com>
2020-05-11 07:52:17 -0500

    Testing/docker worker service (#3)
    * Dockerize and work on sending stop/cancel
    
    * Playing. Not getting far though
    
    * Updated to use Linux containers. Added a test for host OS and call the appropriate method when configuring service.
    Added analyzers and corrected potential issues/recommendations based on this.
    
    * More changes for linux containers

--- Program.cs
+++ Program.cs
@@ -1,21 +1,32 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
+using System.Runtime.InteropServices;
 
 namespace James
 {
-    public class Program
+    public static class Program
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+        public static void Main(string[] args) => CreateHostBuilder(args).Build().Run();
 
-        // TODO: Potential logic for determining linux systemd or windows service and apply:
-        //public static IHostBuilder CreateHostBuilder(string[] args) => args[0].Equals("w")
-        //    ? Host.CreateDefaultBuilder(args).ConfigureServices((hostContext, services) => services.AddHostedService<Worker>()).UseWindowsService()
-        //    : Host.CreateDefaultBuilder(args).ConfigureServices((hostContext, services) => services.AddHostedService<Worker>()).UseConsoleLifetime();
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args).ConfigureServices((hostContext,
-                                                               services) => services.AddHostedService<Worker>()).UseWindowsService();
+        // Determine linux systemd or windows service and apply:
+        public static IHostBuilder CreateHostBuilder(string[] args) => GetHostOsInfo() == OSPlatform.Windows
+            ? Host.CreateDefaultBuilder(args).ConfigureServices((hostContext, services) => services.AddHostedService<Worker>()).UseWindowsService()
+            : Host.CreateDefaultBuilder(args).ConfigureServices((hostContext, services) => services.AddHostedService<Worker>()).UseConsoleLifetime();
+
+        public static OSPlatform GetHostOsInfo()
+        {
+            var currentOs = Environment.OSVersion;
+
+            return RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
+                ? OSPlatform.Windows
+                : RuntimeInformation.IsOSPlatform(OSPlatform.FreeBSD)
+                    ? OSPlatform.FreeBSD
+                    : RuntimeInformation.IsOSPlatform(OSPlatform.Linux)
+                                    ? OSPlatform.Linux
+                                    : RuntimeInformation.IsOSPlatform(OSPlatform.OSX)
+                                                    ? OSPlatform.OSX
+                                                    : OSPlatform.Create("Unknown");
+        }
     }
 }
